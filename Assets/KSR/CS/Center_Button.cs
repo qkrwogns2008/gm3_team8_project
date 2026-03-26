9922f84e7547ef66866490b27a2a1a4de285e6dd
rlatpfkd-wq <rlatpfkd@gmail.com>
2026-03-26 18:42:37 +0900

    feat : 하단 메뉴 마무리
    하단 메뉴 기능 완성입니다.

---
+++ Assets/KSR/CS/Center_Button.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Center_Button : MonoBehaviour
+{
+    [Header("프리펩 off")]
+    public List<GameObject> targetPrefabs = new List<GameObject>();
+
+    [Header("센터버튼 a")]
+    public GameObject onTarget;
+
+    [Header("센터버튼 b")]
+    public GameObject offTarget;
+
+    public void OnClick()
+    {
+        // 1. 프리팹 전부 끄기
+        foreach (var obj in targetPrefabs)
+        {
+            if (obj != null)
+                obj.SetActive(false);
+        }
+
+        // 2. A 켜기
+        if (onTarget != null)
+            onTarget.SetActive(true);
+
+        // 3. B 끄기
+        if (offTarget != null)
+            offTarget.SetActive(false);
+    }
+}
