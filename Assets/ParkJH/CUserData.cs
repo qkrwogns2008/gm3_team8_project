0f36f891acdca94007de64a5dc58f7ab56cd1622
JH0001 <qkrwogns2008@gmail.com>
2026-04-02 11:09:39 +0900

    feat: UserBase(Json), Parallax2D Background

--- Assets/ParkJH/CUserData.cs
+++ Assets/ParkJH/CUserData.cs
@@ -2,24 +2,38 @@ using System;
 using System.Collections.Generic;
 
 [Serializable]
-public class CUserData
+public class CUserData 
 {
     // 재화 및 기본 정보
     public string UserName = "GM3_8Team_FiveKnights_Account";
+    public int UserLevel = 0;
     public int Gold = 0;
     public int Diamond = 0;
+    public int PickUpTicket = 0;
     public int CurrentStage = 1;
+    public int BaseDamage = 1;
+    public int Baseshield = 1;
+    public int BaseHP = 1;
 
-    // 보유 중인 영웅들의 레벨 정보 (영웅 ID : 레벨)
-    public List<HeroSaveData> HeroList = new List<HeroSaveData>();
+    // 보유 중인 영웅ID, 레벨 정보
+    public List<UserHeroData> HeroList = new List<UserHeroData>();
+    // 보유 중인 아이템ID, 개수 정보
+    public List<UserItemData> Inventory = new List<UserItemData>();
 
-    // 마지막 접속 시간 (오프라인 보상 계산용)
-    public string LastLogoutTime = string.Empty;
+    // 마지막 접속 시간
+    public long LastLogoutTime = 0;
 }
 
 [Serializable]
-public class HeroSaveData
+public class UserHeroData
 {
-    public int HeroID;
-    public int Level;
+    public int HeroID;  // 영웅 고유 번호
+    public int Level;   // 영웅 레벨
+}
+
+[Serializable]
+public class UserItemData
+{
+    public int ItemID;     // 아이템 고유 번호
+    public int Quantity;   // 보유 개수
 }
