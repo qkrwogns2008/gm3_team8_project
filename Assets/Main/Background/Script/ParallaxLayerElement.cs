ff3b98e7e202d89cf0ddda15684030bda0446a32
JH0001 <qkrwogns2008@gmail.com>
2026-04-15 00:34:05 +0900

    feat: effect 배경 동기화 작업

--- Assets/Main/Background/Script/ParallaxLayerElement.cs
+++ Assets/Main/Background/Script/ParallaxLayerElement.cs
@@ -4,5 +4,5 @@ public class ParallaxLayerElement : MonoBehaviour
     [Range(-2f, 2f)]
     public float factorX; // 이 레이어의 이동 비율
     [Range(-2f, 2f)]
-    public float factorY; // 이 레이어의 이동 비율
+    public float factorY = 0.0095f; // 이 레이어의 이동 비율
 }
