1435ae6db10be894542573915f12594eea18ea5e
hwoo <greenapple914@gmail.com>
2026-04-14 14:51:42 +0900

    fix: joystick

--- Assets/ParkJH/Script/TestHeroAdd.cs
+++ Assets/ParkJH/Script/TestHeroAdd.cs
@@ -11,12 +11,14 @@ public class TestHeroAdd : MonoBehaviour
     IEnumerator Start()
     {
         yield return null; // 한 프레임 대기 (모든 Awake/Start 완료 보장)
+        /*
         CDataManager.Instance.AddUserHeroArray(0, 0, EHeroID.Evan);
         CDataManager.Instance.AddUserHeroArray(0, 3, EHeroID.Teo);
         CDataManager.Instance.AddUserHeroArray(1, 1, EHeroID.Radgrid);
         CDataManager.Instance.AddUserHeroArray(1, 2, EHeroID.Shane);
         CDataManager.Instance.AddUserHeroArray(1, 3, EHeroID.Sarah);
         CDataManager.Instance.AddUserHeroArray(3, 3, EHeroID.Elga);
+        */
         CDataManager.Instance.AddHeroData(EHeroID.Baskin);
         CDataManager.Instance.AddHeroData(EHeroID.Nami);
         CDataManager.Instance.AddHeroData(EHeroID.Loto);
