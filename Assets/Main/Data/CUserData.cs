b413d08b76ebccfe0f4b47ef0f8716602a9a34e7
com-wj <a7533806@gmail.com>
2026-04-21 09:15:02 +0900

    fix: 초기 유저 데이터 세팅
    골드, 다이아몬드, 루비 10000 → 0
    픽업 티켓 10000 → 60

--- Assets/Main/Data/CUserData.cs
+++ Assets/Main/Data/CUserData.cs
@@ -13,14 +13,14 @@ public class CUserData
     public string UserName = "GM3_8Team_FiveKnights_Account";
     public int UserLevel = 100;
     // 재화 관련 함수
-    public int Gold = 10000;
-    public int Diamond = 10000; // 현금 재화
+    public int Gold = 0;
+    public int Diamond = 0; // 현금 재화
     public int goldDungeonTicket = 5;
     public int expDungeonTicket = 5;
     public int expPoint = 10000;
     // 픽업 관련 함수
-    public int Ruby = 10000;
-    public int PickUpTicket = 10000;
+    public int Ruby = 0;
+    public int PickUpTicket = 60;
     public int HeroPickUpLevel = 0;
     public int PetPickUpLevel = 0;
     // 스테이지 관련 함수
